9cbf4e32d897f1fa9991eea1e7c7a5725c1c8b2f
Leo <lcheung@swellbottle.com>
2022-03-27 21:03:42 -0400

    explicit component type annotation

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -7,3 +7,12 @@ export const Child = ({ color }: ChildProps) => {
         <div>{color}</div>
     )
 }
+
+// function component with typescript
+// tell Ttypescript this is a react functional component
+// this function will receive ChildProps
+// this is better as intellisense showing properties
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+    return <div>{color}</div>
+
+}
