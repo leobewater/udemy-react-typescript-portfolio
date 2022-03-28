67d20e77c46615b13363481a87e8a66ec1eda9e0
Leo <lcheung@swellbottle.com>
2022-03-28 11:16:33 -0400

    update format

--- 02-redux/src/state/reducers/repositoriesReducer.ts
+++ 02-redux/src/state/reducers/repositoriesReducer.ts
@@ -9,14 +9,20 @@ interface Action {
   payload?: any // optional paypload with any type
 }
 
-const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
   switch (action.type) {
     case 'search_repositories':
       return { loading: true, error: null, data: [] }
+
     case 'search_repositories_success':
       return { loading: false, error: null, data: action.payload }
+
     case 'search_repositories_error':
       return { loading: false, error: action.payload, data: [] }
+
     default:
       return state
   }
