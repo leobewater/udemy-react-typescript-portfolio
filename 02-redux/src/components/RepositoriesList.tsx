b2c92eea7c2b2f51dae1d168ff50cf0df77e9cd8
Leo <lcheung@swellbottle.com>
2022-03-28 14:49:02 -0400

    add TypedUseSelectorHook

--- 02-redux/src/components/RepositoriesList.tsx
+++ 02-redux/src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import { useActions } from '../hooks/useActions'
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('')
     const { searchRepositories } = useActions()
+
+    // get repositories from state
+    const { data, error, loading } = useSelector((state) => state.repositories)
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
