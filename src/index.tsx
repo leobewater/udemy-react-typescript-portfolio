3e5654f799f6597539a8ee945d3d6a40a710b188
Leo <lcheung@swellbottle.com>
2022-03-27 21:24:31 -0400

    add usersearch component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import GuestList from './state/GuestList'
+import UserSearch from './state/UserSearch'
 
 const App = () => {
     return <div>
-        <GuestList />
+        <UserSearch />
     </div>
 }
 
