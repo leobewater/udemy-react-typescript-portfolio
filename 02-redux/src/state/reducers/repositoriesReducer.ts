cce4fbd54559781983dd6de215a0bd321a408832
Leo <lcheung@swellbottle.com>
2022-03-28 11:06:22 -0400

    add repositories reducer

---
+++ 02-redux/src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,8 @@
+interface RepositoriesState {
+  loading: boolean
+  error: string | null
+  data: string[]
+}
+
+const reducer = (state: RepositoriesState, action: any) => {}
+export default reducer
